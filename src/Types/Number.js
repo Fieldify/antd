473db887f22880b4b32b4f6882f19c7192ead3d0
Michael Vergoz <m.vergoz@vergoz.ch>
2020-05-29 22:56:17 +0200

    Add Number type

---
+++ src/Types/Number.js
@@ -0,0 +1,90 @@
+
+import { types } from 'fieldify'
+
+import React from 'react';
+
+import {
+  InputNumber,
+  Form,
+  Space,
+  Tag,
+  Input,
+  Col,
+  Row
+} from "antd";
+
+import { NumberOutlined as Icon } from '@ant-design/icons';
+
+import TypeForm from '../lib/TypeForm';
+import TypeInfo from '../lib/TypeInfo';
+import TypeBuilder from '../lib/TypeBuilder';
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *
+ * Rendering of the field in a form
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+class NumberForm extends TypeForm {
+  render() {
+    return (super.render(
+      <InputNumber 
+      value={this.state.value} 
+      placeholder={this.state.options.placeholder} 
+      onChange={(value) => this.changeValue(value)} 
+      style={{width: "100%"}}
+      />
+    ));
+  }
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *
+ * Information of the field show fancy information of the type
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+class NumberInfo extends TypeInfo {
+  render() {
+    return (
+      <span>
+        <Tag color="#ff7a45"><Icon /></Tag>
+      </span>
+    )
+  }
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *
+ * Field builder
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+class NumberBuilder extends TypeBuilder {
+  constructor(props) {
+    super(props)
+
+    this.default = {
+      minSize: 1,
+      maxSize: 128
+    }
+
+    this.configure()
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default {
+  code: types.Number.code,
+  description: types.Number.description,
+  class: types.Number.class,
+
+  Info: NumberInfo,
+  Builder: NumberBuilder,
+  Form: NumberForm
+}
+
+

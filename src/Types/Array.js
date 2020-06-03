6636ed93dce0a80fd1c39ec6556c5c646d2f4ec1
Michael Vergoz <m.vergoz@vergoz.ch>
2020-06-03 13:54:15 +0200

    Add object and array virtual type

---
+++ src/Types/Array.js
@@ -0,0 +1,29 @@
+// this is a hidden type
+// to manage nested object
+import { types,  fieldifyType } from 'fieldify'
+
+
+class ArrayClass extends fieldifyType {
+  configuration () {
+    return ({
+      min: {
+        $doc: 'Minimum of items',
+        $required: false,
+        $type: 'Number'
+      },
+      max: {
+        $doc: 'Maximun of items',
+        $required: false,
+        $type: 'Number'
+      }
+    })
+  }
+
+}
+
+
+export default {
+  code: "Array",
+  description: "Array",
+  class: ArrayClass
+}
---
+++ src/Types/Object.js
@@ -0,0 +1,16 @@
+// this is a hidden type
+// to manage nested object
+import { types,  fieldifyType } from 'fieldify'
+
+
+class ObjectClass extends fieldifyType {
+
+
+}
+
+
+export default {
+  code: "Object",
+  description: "Nested Sub Object",
+  class: ObjectClass
+}

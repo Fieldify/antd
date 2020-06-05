bb5e0fa739271a40067fa6480e99bdab630fcfd6
Michael Vergoz <m.vergoz@vergoz.ch>
2020-06-05 11:34:16 +0200

    Refactoring schema input reader

--- src/Schema/Render.js
+++ src/Schema/Render.js
@@ -9,200 +9,26 @@ import {
 } from '@ant-design/icons';
 
 import { FieldifySchema } from "../Schema/Schema";
+import { TypeDataset } from "../lib/TypeDataset";
 
 export class FieldifySchemaRender extends RecycledComponent {
-  constructor(props) {
-    super(props)
-
-    this.formRef = React.createRef()
-  }
-
-  cycle(props, first) {
-
+  cycle(props) {
     const state = {
-      layout: props.layout ? props.layout : "horizontal"
+      layout: props.layout,
+      schema: props.schema,
+      input: props.input,
     }
 
-    // compile the schema
-    state.rawSchema = props.schema
-    state.schema = new FieldifySchema("form")
-    state.schema.compile(state.rawSchema)
-
-    // create an input instance
-    // console.log("rawInput", state.rawInput === props.rawInput)
-    state.rawInput = props.input
-    state.input = new FieldifyInput(state.schema)
-    state.input.setValue(props.input)
-    state.inputValue = state.input.getValue()
-
-    state.verify = props.verify || false
-
-    this.references = {};
-
-    this.onChange = props.onChange ? props.onChange : () => { };
-
-    state.reactive = this.update(state.schema, state.inputValue, state.verify);
-
-    return (state)
-  }
-
-  update(root, input, verify) {
-    const follower = (schema, input, ret, line) => {
-      line = line || ""
-
-      utils.orderedRead(schema, (index, item) => {
-        const source = { ...Array.isArray(item) ? item[0] : item };
-        const inputPtr = input ? input[source.$_key] : null;
-        const lineKey = line + "." + source.$_key;
-
-        if (source.$_array === true) {
-          const columns = [
-            {
-              dataIndex: 'form',
-              key: 'form',
-              width: "100%"
-            }
-          ];
-
-          const dataSource = []
-
-          var inputPtr2 = inputPtr;
-          const options = source.$array || {};
-          const min = options.min ? options.min : (source.$required === true ? 1 : 0)
-
-          if (source.$_nested === true) {
-            var inputPtr2 = input[source.$_key]
-
-            if (!Array.isArray(inputPtr)) inputPtr2 = input[source.$_key] = [];
-
-            // force to create min form
-            if (min - inputPtr2.length > 0) {
-              for (var a = 0; a <= min - inputPtr2.length; a++) {
-                inputPtr2.push({})
-              }
-            }
-
-            for (var a = 0; a < inputPtr2.length; a++) {
-              const value = inputPtr2[a];
-              const key = lineKey + "." + a
-
-              const child = [];
-              follower(source, value, child, key);
-
-              dataSource.push({
-                key,
-                form: child
-              })
-            }
-          }
-          else if(source.$type) {
-            delete source.$doc; // source is cloned
-            const TypeRender = source.$type.Render;
-
-            // console.log("Array non nested", min, inputPtr2)
-
-            if (verify === true) {
-              // console.log()
-            }
-
-            if (TypeRender) {
-              if (!Array.isArray(inputPtr)) {
-                input[item.$_key] = [];
-                inputPtr2 = input[item.$_key];
-              }
-
-              if (!inputPtr2) return (ret);
-
-              if (min - inputPtr2.length > 0) {
-                for (var a = 0; a <= min - inputPtr2.length; a++) {
-                  inputPtr2.push(null)
-                }
-              }
-
-              for (var a = 0; a < inputPtr2.length; a++) {
-                const value = inputPtr2[a];
-                const key = lineKey + "." + a
-
-                dataSource.push({
-                  key,
-                  form: <TypeRender
-                    schema={source}
-                    value={value}
-                    injected={true}
-                    key={"render."+source.$_wire}
-                  />
-                })
-              }
-
-            }
-
-          }
-
-          ret.push(<Form.Item key={source.$_wire} noStyle={true}>
-            <div className="ant-form-item">
-              <Card size="small" title={source.$_access.$doc}>
-                <Table
-                  size="small"
-                  dataSource={dataSource}
-                  columns={columns}
-                  showHeader={false}
-                  pagination={{
-                    total: dataSource.length,
-                    pageSize: dataSource.length,
-                    hideOnSinglePage: true
-                  }}
-                />
-              </Card>
-            </div>
-          </Form.Item>);
-
-        }
-        else {
-          if (source.$_nested === true) {
-            const child = [];
-            follower(source, inputPtr, child, lineKey);
-
-            ret.push(<div key={"render."+source.$_wire} className="ant-form-item">
-              <Card size="small" title={source.$doc}>
-                {child}
-              </Card>
-            </div>);
-          }
-          else {
-            const TypeRender = item.$type.Render;
-            if (TypeRender) {
-              ret.push(<TypeRender
-                schema={source}
-                value={inputPtr}
-                key={"render."+source.$_wire}
-              />);
-            }
-          }
-        }
-      });
-      return (ret);
-    };
-
-    const ret = [];
-    follower(root.handler.schema, input, ret);
-    return (ret);
+    return(state)
   }
 
   render() {
-    var layout = {};
-
-    if (this.state.layout === 'horizontal') {
-      layout = {
-        labelCol: { span: 8 },
-        wrapperCol: { span: 16 },
-      }
-    }
-    return (<Form
+    return (<TypeDataset
+      schema={this.state.schema}
+      input={this.state.input}
+      actions={false}
       layout={this.state.layout}
-      key={this.formRef}
-      {...layout}
-      name="basic" >
-      {this.state.reactive}
-    </Form>);
+      generator="Render"
+    />);
   }
 }
---
+++ src/lib/TypeDataset.js
@@ -0,0 +1,334 @@
+import { utils, input as FieldifyInput } from "fieldify";
+import React from 'react';
+import RecycledComponent from 'react-recycling';
+
+import { Form, Table, Button, Input, Card } from "antd";
+import {
+  PlusOutlined as PlusIcon,
+  DeleteOutlined as DeleteIcon
+} from '@ant-design/icons';
+
+import { FieldifySchema } from "../Schema/Schema";
+
+
+export class TypeDataset extends RecycledComponent {
+  constructor(props) {
+    super(props)
+
+    this.formRef = React.createRef()
+
+  }
+
+  cycle(props, first) {
+
+    const state = {
+      layout: props.layout ? props.layout : "horizontal"
+    }
+
+    // compile the schema
+    state.rawSchema = props.schema
+    state.schema = new FieldifySchema("form")
+    state.schema.compile(state.rawSchema)
+
+    // create an input instance
+    // console.log("rawInput", state.rawInput === props.rawInput)
+    state.rawInput = props.input
+    state.input = new FieldifyInput(state.schema)
+    state.input.setValue(props.input)
+
+    state.inputValue = state.input.getValue()
+    state.verify = props.verify || false
+
+    state.actions = props.actions;
+    state.generator = props.generator;
+
+    this.references = {};
+
+    this.onChange = props.onChange ? props.onChange : () => { };
+
+    // state.reactive = this.update(state.schema, state.inputValue, state.verify);
+    state.reactive = this.update({
+      state,
+      root: state.schema,
+      input: state.inputValue,
+      verify: state.verify
+    })
+    return (state)
+  }
+
+  getValue() {
+    return (this.state.input.getValue())
+  }
+
+  clickAddArray(line) {
+    this.state.input.set(line)
+    const _value = this.state.input.getValue();
+    this.onChange(this.state.input, _value)
+    this.setState({
+      inputValue: _value,
+      // reactive: this.update(this.state.schema, _value, false)
+      reactive: this.update({
+        state: this.state,
+        root: this.state.schema,
+        input: _value,
+        verify: false,
+      })
+    })
+  }
+
+  clickRemoveArrayItem(line) {
+    this.state.input.remove(line)
+    const _value = this.state.input.getValue();
+    this.onChange(this.state.input, _value)
+    this.setState({
+      inputValue: _value,
+      // reactive: this.update(this.state.schema, _value, false)
+
+      reactive: this.update({
+        state: this.state,
+        root: this.state.schema,
+        input: _value,
+        verify: false,
+      })
+
+    })
+  }
+
+
+  setValue(line, value) {
+    if (!this.state.input) return;
+
+    this.state.input.set(line, value)
+    const _value = this.state.input.getValue();
+    this.onChange(this.state.input, _value)
+    this.setState({
+      inputValue: _value
+    })
+  }
+
+  update(up) {
+
+    var { root, input, verify, state } = up;
+
+    const follower = (schema, schematized, input, ret, line) => {
+      line = line || ""
+
+      if (!input) input = {}
+
+      utils.orderedRead(schema, (index, item) => {
+
+        const source = { ...Array.isArray(item) ? item[0] : item };
+        const schematizedSrc = schematized[source.$_key];
+        const sourceSchematized = { ...Array.isArray(schematizedSrc) ? schematizedSrc[0] : schematizedSrc };
+
+        const inputPtr = input ? input[source.$_key] : null;
+        const lineKey = line + "." + source.$_key;
+
+        if (source.$_array === true) {
+          const columns = [
+            {
+              dataIndex: 'form',
+              key: 'form',
+              width: "100%"
+            }
+          ];
+
+          if (state.actions === true) {
+            columns.push({
+              dataIndex: 'actions',
+              key: 'actions',
+              align: "right"
+            })
+          }
+
+          const dataSource = []
+
+          var inputPtr2 = inputPtr;
+          const options = source.$array || {};
+          const min = options.min ? options.min : (source.$required === true ? 1 : 0)
+
+          if (source.$_nested === true) {
+            var inputPtr2 = input[source.$_key]
+
+            if (!Array.isArray(inputPtr)) inputPtr2 = input[source.$_key] = [];
+
+            // force to create min form
+            if (min - inputPtr2.length > 0) {
+              for (var a = 0; a <= min - inputPtr2.length; a++) {
+                inputPtr2.push({})
+              }
+            }
+
+            for (var a = 0; a < inputPtr2.length; a++) {
+              const value = inputPtr2[a];
+              const key = lineKey + "." + a
+
+              const child = [];
+              follower(source, sourceSchematized, value, child, key);
+
+              const toPush = { key, form: child }
+              if (state.actions === true) {
+                toPush.actions = <Button size="small" onClick={() => this.clickRemoveArrayItem(key)}>
+                  <span><DeleteIcon /></span>
+                </Button>
+              }
+              dataSource.push(toPush)
+            }
+          }
+          else if (source.$type) {
+            delete sourceSchematized.$doc; // source is cloned
+            const TypeForm = source.$type[state.generator];
+
+            // console.log("Array non nested", min, inputPtr2)
+
+            if (verify === true) {
+              // console.log()
+            }
+
+            if (!Array.isArray(inputPtr)) {
+              input[source.$_key] = [];
+              inputPtr2 = input[source.$_key];
+            }
+
+            if (!inputPtr2) return (ret);
+
+            if (min - inputPtr2.length > 0) {
+              for (var a = 0; a <= min - inputPtr2.length; a++) {
+                inputPtr2.push(null)
+              }
+            }
+
+            for (var a = 0; a < inputPtr2.length; a++) {
+              const value = inputPtr2[a];
+              const key = lineKey + "." + a
+
+              const toPush = {
+                key,
+                form: <TypeForm
+                  schema={sourceSchematized}
+                  value={value}
+                  verify={verify}
+                  user={this.props.user}
+                  onChange={(schema, value) => this.setValue(key, value)}
+                  isInjected={true}
+
+                  // reference errors
+                  onError={(error, message) => {
+                    if (error === true) {
+                      this.references[key] = message;
+                    }
+                    else {
+                      const ref = this.references[key];
+                      if (ref) {
+                        delete this.references[key];
+                      }
+                    }
+                  }}
+                />
+              }
+              if (state.actions === true) {
+                toPush.actions = <Button size="small" onClick={() => this.clickRemoveArrayItem(key)}>
+                  <span><DeleteIcon /></span>
+                </Button>
+              }
+              dataSource.push(toPush)
+            }
+          }
+
+          ret.push(<Form.Item key={source.$_wire} noStyle={true}>
+            <div className="ant-form-item">
+              <Card size="small" title={source.$_access.$doc} extra={<div className="ant-radio-group ant-radio-group-outline ant-radio-group-small">
+                {inputPtr2 && state.actions === true ?
+                  <span className="ant-radio-button-wrapper" onClick={() => this.clickAddArray(lineKey + "." + inputPtr2.length)}>
+                    <span><PlusIcon /></span>
+                  </span>
+                  : null}
+              </div>}>
+                <Table
+                  size="small"
+                  dataSource={dataSource}
+                  columns={columns}
+                  verticalAlign='middle'
+                  showHeader={false}
+                  pagination={{
+                    total: dataSource.length,
+                    pageSize: dataSource.length,
+                    hideOnSinglePage: true
+                  }}
+                  bordered
+                />
+              </Card>
+            </div>
+          </Form.Item>);
+
+        }
+        else {
+          if (source.$_nested === true) {
+            const child = [];
+
+            follower(source, sourceSchematized, inputPtr, child, lineKey);
+
+            ret.push(<div key={source.$_wire} className="ant-form-item">
+              <Card size="small" title={source.$doc}>
+                {child}
+              </Card>
+            </div>);
+          }
+          else if (item.$type) {
+            const TypeForm = item.$type[state.generator];
+
+            ret.push(<TypeForm
+              schema={sourceSchematized}
+              value={inputPtr}
+              key={source.$_wire}
+              verify={verify}
+              user={this.props.user}
+              onChange={(schema, value) => this.setValue(lineKey, value)}
+
+              // reference errors
+              onError={(error, message) => {
+                if (error === true) {
+                  this.references[source.$_wire] = message;
+                }
+                else {
+                  const ref = this.references[source.$_wire];
+                  if (ref) {
+                    delete this.references[source.$_wire];
+                  }
+                }
+              }}
+            />);
+          }
+        }
+      });
+      return (ret);
+    };
+
+    const ret = [];
+    follower(
+      root.handler.schema,
+      root.handlerSchematized.schema,
+      input,
+      ret
+    );
+    return (ret);
+  }
+
+  render() {
+    var layout = {};
+
+    if (this.state.layout === 'horizontal') {
+      layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+      }
+    }
+    return (<Form
+      layout={this.state.layout}
+      key={this.formRef}
+      {...layout}
+      name="basic" >
+      {this.state.reactive}
+    </Form>);
+  }
+}

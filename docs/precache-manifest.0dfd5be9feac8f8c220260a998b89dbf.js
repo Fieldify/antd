c4a20c9f45759ee60714a1a717c34ffe28994471
Michael Vergoz <m.vergoz@vergoz.ch>
2020-07-28 15:27:24 +0200

    Update doc code

---
+++ docs/precache-manifest.0dfd5be9feac8f8c220260a998b89dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6981ca7735d6af3c420869bb2add2f5",
+    "url": "/antd/index.html"
+  },
+  {
+    "revision": "c311448edc49daee00c9",
+    "url": "/antd/static/css/2.e7180fea.chunk.css"
+  },
+  {
+    "revision": "627f084c73e659f6777b",
+    "url": "/antd/static/css/main.f72a47cc.chunk.css"
+  },
+  {
+    "revision": "c311448edc49daee00c9",
+    "url": "/antd/static/js/2.0294653c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/antd/static/js/2.0294653c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627f084c73e659f6777b",
+    "url": "/antd/static/js/main.57a70e25.chunk.js"
+  },
+  {
+    "revision": "da6ec5edfc2736e11352",
+    "url": "/antd/static/js/runtime-main.161f7dc1.js"
+  }
+]);
